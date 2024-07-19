aa641dd2072f39b588253527462a343b609ac0a7
Diego Maia <ldgmaia@gmail.com>
2024-07-18 21:06:32 -0400

    Initial commit

---
+++ src/app/components/header.tsx
@@ -0,0 +1,16 @@
+import facebookIcon from '@/app/assets/facebook-icon.svg'
+import instagramIcon from '@/app/assets/instagram-icon.svg'
+import twitterIcon from '@/app/assets/twitter-icon.svg'
+import whatsappIcon from '@/app/assets/whatsapp-icon.svg'
+import Image from 'next/image'
+
+export async function Header() {
+  return (
+    <div className='mx-auto flex max-w items-center justify-center border-b pb-2 gap-2 py-3 bg-[#0F2755]'>
+      <Image src={facebookIcon} className='size-9' alt='Rocketseat' />
+      <Image src={instagramIcon} className='size-9' alt='Rocketseat' />
+      <Image src={twitterIcon} className='size-9' alt='Rocketseat' />
+      <Image src={whatsappIcon} className='size-9' alt='Rocketseat' />
+    </div>
+  )
+}
